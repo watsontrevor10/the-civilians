8bcbed8772337d2e4fca01c9dfc65d22ecd05172
Trevor Watson <watsontrevor@gmail.com>
2021-06-26 16:09:28 -0600

    add sticky navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <b-navbar sticky="true">
+    <b-navbar-brand>
+      The Civilians
+    </b-navbar-brand>
+    <b-navbar-nav>
+      <b-nav-item href="#">Music</b-nav-item>
+      <b-nav-item href="#">About</b-nav-item>
+      <b-nav-item href="#">Book Us</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
